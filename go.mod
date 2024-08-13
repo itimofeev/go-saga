922144689dd97c1ea74f8110c2b4859a4e11f4ab
jaume.bret <jaume.bret@tangelogames.com>
2024-08-13 12:03:14 +0200

    Add a new gorm ORM log store.

--- go.mod
+++ go.mod
@@ -1,8 +1,21 @@
-module github.com/itimofeev/go-saga
+module github.com/tangelo-labs/go-saga
 
-go 1.13
+go 1.21
+
+toolchain go1.22.6
 
 require (
-	github.com/sanity-io/litter v1.1.0
-	github.com/stretchr/testify v1.3.0
+	github.com/brianvoe/gofakeit/v6 v6.28.0
+	github.com/stretchr/testify v1.9.0
+	gorm.io/driver/sqlite v1.5.6
+	gorm.io/gorm v1.25.11
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-sqlite3 v1.14.22 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/text v0.17.0 // indirect
 )
---
+++ gorm.go
@@ -0,0 +1,40 @@
+package saga
+
+import (
+	"context"
+	"gorm.io/gorm"
+)
+
+type gormStore struct {
+	db *gorm.DB
+}
+
+func NewGormStore(db *gorm.DB) Store {
+	return &gormStore{db: db}
+}
+
+func (g *gormStore) AppendLog(ctx context.Context, log *Log) error {
+	return g.db.WithContext(ctx).Create(log).Error
+}
+
+func (g *gormStore) GetAllLogsByExecutionID(ctx context.Context, executionID string) ([]*Log, error) {
+	logs := make([]*Log, 0)
+
+	err := g.db.WithContext(ctx).Where("execution_id = ?", executionID).Find(&logs).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return logs, nil
+}
+
+func (g *gormStore) GetStepLogsToCompensate(ctx context.Context, executionID string) ([]*Log, error) {
+	logs := make([]*Log, 0)
+
+	err := g.db.WithContext(ctx).Where("execution_id = ? AND type = ?", executionID, LogTypeSagaStepExec).Find(&logs).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return logs, nil
+}
